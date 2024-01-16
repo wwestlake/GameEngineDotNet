32d053a6aad5b313e60c393edeec61bc7ce6befe
William Westlake <wwestlake@lagdaemon.com>
2024-01-16 11:11:25 -0500

    adding python

--- Experimental/Services/IPythonService.cs
+++ Experimental/Services/IPythonService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Experimental.Services
 {
-    internal interface IPythonService
+    public interface IPythonService
     {
     }
 }
--- Experimental/Services/PythonService.cs
+++ Experimental/Services/PythonService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Scripting.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,15 @@ using System.Threading.Tasks;
 
 namespace Experimental.Services
 {
-    internal class PythonService
+    public class PythonService
     {
+        private ScriptEngine _engine;
+        private ScriptScope _scope;
+
+        public PythonService(IConfigurationRoot config)
+        {
+            _engine = IronPython.Hosting.Python.CreateEngine();
+            _scope = _engine.CreateScope();
+        }
     }
 }
